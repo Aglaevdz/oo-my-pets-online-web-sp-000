5115ce8d0f44fd8a4c8bdc24524b7df0dfe5a13f
Aglae Verduzco <aglae.cv@gmail.com>
2019-03-17 00:24:31 +0000

    Automatically backed up by Learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,7 +1,12 @@
 class Fish
-  attr_accessor :name
+  attr_reader :name, :mood
 
   def initialize(name)
     @name = name
+    @mood = "nervous"
+  end
+
+  def mood=(mood)
+    @mood = mood
   end
 end
